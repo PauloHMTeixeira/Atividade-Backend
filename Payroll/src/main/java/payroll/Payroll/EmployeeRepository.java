cab117b24d2f0d6d877bf76fbba35186109ab91d
unknown <TEMP@MEREIDAS-011044.cesar-ldap.intranet>
2022-08-31 10:02:36 -0300

    initial commit, project structure with its ininitial classes

---
+++ Payroll/src/main/java/payroll/Payroll/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package payroll.Payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
