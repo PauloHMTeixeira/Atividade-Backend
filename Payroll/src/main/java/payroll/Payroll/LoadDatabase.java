9f31dec1833c30cf857438608e8d5ddaf15bace4
unknown <TEMP@MEREIDAS-011044.cesar-ldap.intranet>
2022-09-05 09:35:17 -0300

    atividade finalizada

--- Payroll/src/main/java/payroll/Payroll/LoadDatabase.java
+++ Payroll/src/main/java/payroll/Payroll/LoadDatabase.java
@@ -17,6 +17,16 @@ class LoadDatabase {
     return args -> {
       log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
       log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "thief")));
+      
+      employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+
+      
+      orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+      orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+      orderRepository.findAll().forEach(order -> {
+        log.info("Preloaded " + order);
+      }
     };
   }
 }
